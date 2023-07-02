b50744d0b7fe6704bbb0cfacbdab8e7b91372e91
yellowsea <yunhai0644@163.com>
2023-07-02 21:51:22 +0800

    chore: dev add VueDevTools

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,11 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import VueDevTools from "vite-plugin-vue-devtools";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VueDevTools(),
     vue(),
     vueJsx(),
     AutoImport({
